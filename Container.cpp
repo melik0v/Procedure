d4fe99cef4be6e7a9e00c70b8a50bb85f0eef27b
melik0v <rmelik00v@gmail.com>
2022-03-17 13:01:49 +0700

    Added sort function

--- Container.cpp
+++ Container.cpp
@@ -1,6 +1,7 @@
 #include "Container.h"
 #include "Plant.h"
 #include <fstream>
+#include <iostream>
 using namespace std;
 namespace Melikov
 {
@@ -59,6 +60,7 @@ namespace Melikov
 
 	void LinkedCircledList_Out(LinkedCircledList& obj, ofstream& fout)
 	{
+		sort(obj);
 		Node* Temp = obj.First;
 		fout << "List contains " << obj.SizeList << " elements" << endl;
 		
@@ -67,8 +69,48 @@ namespace Melikov
 			Out(*(Temp->plant), fout);
 			fout << "Consonants = " << consonants(*(Temp->plant)) << endl;
 			Temp = Temp->Next;
+			
 		}
 		fout << endl;	
 	}
 
+	bool compare(plant* first, plant* second) {
+		return consonants(*first) > consonants(*second);
+	}
+
+	void sort(LinkedCircledList& obj) {
+		if (obj.SizeList < 2) {
+			return;
+		}
+
+		Node* current = obj.First;
+		bool flag = false;
+
+		do
+		{
+			current = obj.First;
+			flag = false;
+			for (int i = 0; i < (obj.SizeList - 1); ++i) 
+			{
+				if (compare(current->plant, current->Next->plant)) 
+				{
+					swap(current, current->Next);
+					flag = true;
+				}
+				else
+				{
+					current = current->Next;
+				}
+			}
+		} while (flag);
+	}
+
+	void swap(Node* first, Node* second) {
+
+		plant* tmp;
+		tmp = first->plant;
+		first->plant = second->plant;
+		second->plant = tmp;
+		return;
+	}
 }
