3539cafd15fa9d576a8da1b4da4c32e33994930f
melik0v <rmelik00v@gmail.com>
2022-04-07 12:46:41 +0700

    Temp renaimed to temp

--- Container.cpp
+++ Container.cpp
@@ -15,12 +15,12 @@ namespace Melikov
 
 	void Clear(LinkedCircledList& obj)
 	{
-		Node* Temp = NULL;
+		Node* temp = NULL;
 		while (obj._sizeList > 1)
 		{
-			Temp = obj._last->_prev;
+			temp = obj._last->_prev;
 			delete obj._last;
-			obj._last = Temp;
+			obj._last = temp;
 			obj._sizeList--;
 		}
 		if (obj._sizeList == 1)
@@ -32,30 +32,30 @@ namespace Melikov
 
 	void LinkedCircledListIn(LinkedCircledList& obj, ifstream& fin)
 	{
-		Node* Temp;
+		Node* temp;
 		while (!fin.eof())
 		{
-			Temp = new Node;
-			Temp->_next = NULL;
-			Temp->_plant = In(fin);
+			temp = new Node;
+			temp->_next = NULL;
+			temp->_plant = In(fin);
 
-			if (Temp->_plant == NULL)
+			if (temp->_plant == NULL)
 			{
 				break;
 			}
 
 			if (obj._first == NULL)
 			{
-				obj._first = obj._last = Temp;
-				Temp->_prev = Temp;
-				Temp->_next = obj._first;
+				obj._first = obj._last = temp;
+				temp->_prev = temp;
+				temp->_next = obj._first;
 			}
 			else
 			{
-				Temp->_prev = obj._last;
-				obj._last->_next = Temp;
-				Temp->_next = obj._first;
-				obj._last = Temp;
+				temp->_prev = obj._last;
+				obj._last->_next = temp;
+				temp->_next = obj._first;
+				obj._last = temp;
 			}
 			obj._sizeList++;
 		}
@@ -64,20 +64,20 @@ namespace Melikov
 	void LinkedCircledListOut(LinkedCircledList& obj, ofstream& fout)
 	{
 
-		Node* Temp = obj._first;
+		Node* temp = obj._first;
 		fout << "List contains " << obj._sizeList << " elements" << endl;
 
 		for (int i = 0; i < obj._sizeList; i++)
 		{
-			Out(*(Temp->_plant), fout);
-			fout << "Consonants = " << Consonants(*(Temp->_plant)) << endl;
-			Temp = Temp->_next;
+			Out(*(temp->_plant), fout);
+			fout << "Consonants = " << Consonants(*(temp->_plant)) << endl;
+			temp = temp->_next;
 		}
 		fout << endl;
 		for (int i = 0; i < obj._sizeList; i++)
 		{
-			OutTree(*(Temp->_plant), fout);
-			Temp = Temp->_next;
+			OutTree(*(temp->_plant), fout);
+			temp = temp->_next;
 		}
 		fout << endl;
 	}
@@ -118,10 +118,10 @@ namespace Melikov
 
 	void Swap(Node* first, Node* second) 
 	{
-		Plant* tmp;
-		tmp = first->_plant;
+		Plant* temp;
+		temp = first->_plant;
 		first->_plant = second->_plant;
-		second->_plant = tmp;
+		second->_plant = temp;
 		return;
 	}
 }
